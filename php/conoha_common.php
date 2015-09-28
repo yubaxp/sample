5875a1540ce0e1fabc68ccb2c93a6fc5f0fb54f6
yuba <yuba@yubax.com>
2015-09-28 21:22:59 +0900

    conoha_dns

--- php/conoha_common.php
+++ php/conoha_common.php
@@ -5,3 +5,39 @@ define("API_TENANT_ID", "テナントID");
 
 define("API_IDENTITY_SERVICE", "https://identity.tyo1.conoha.io/v2.0");
 define("API_DNS_SERVICE", "https://dns-service.tyo1.conoha.io");
+
+/**
+ * トークン取得
+ * @param string $options
+ */
+function getToken($options = null)
+{
+	$url = API_IDENTITY_SERVICE . "/tokens";
+
+	$headers = array(
+		"Accept: application/json"
+	);
+
+	$req_data = array(
+		"auth" => array(
+			"passwordCredentials" => array(
+				"username" => API_USERNAME,
+				"password" => API_PASSWORD
+			),
+			"tenantId" => API_TENANT_ID
+		)
+	);
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($req_data));
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	$res_json =  curl_exec($ch);
+	$res_data = json_decode($res_json);
+	curl_close($ch);
+
+	return $res_data;
+}
--- php/conoha_dns.php
+++ php/conoha_dns.php
@@ -2,42 +2,6 @@
 require_once 'conoha_common.php';
 
 /**
- * トークン取得
- * @param string $options
- */
-function getToken($options = null)
-{
-	$url = API_IDENTITY_SERVICE . "/tokens";
-
-	$headers = array(
-		"Accept: application/json"
-	);
-
-	$req_data = array(
-		"auth" => array(
-			"passwordCredentials" => array(
-				"username" => API_USERNAME,
-				"password" => API_PASSWORD
-			),
-			"tenantId" => API_TENANT_ID
-		)
-	);
-
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($req_data));
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	$res_json =  curl_exec($ch);
-	$res_data = json_decode($res_json);
-	curl_close($ch);
-
-	return $res_data;
-}
-
-/**
  * ドメイン一覧取得
  * @param unknown $token
  * @param string $options
